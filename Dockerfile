5b2d3b2112b869af40d814fae7c5c6cd46092216
Suriya Kalivardhan <suriyak@microsoft.com>
2024-04-17 15:03:24 -0700

    Docer file changes to pull latest image

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN apt update -y; apt install vim -y
 WORKDIR /usr/src/app
 
 RUN git clone https://github.com/SuriyaKalivardhan/llm-runners.git .
-COPY requirements.txt ./
+RUN git fetch -p
+RUN git pull
 RUN pip install --no-cache-dir -r requirements.txt
--- scripts.sh
+++ scripts.sh
@@ -2,4 +2,6 @@ az configure -d subscription=ea4faa5b-5e44-4236-91f6-5483d5b17d14 group=llm-runn
 az login --use-device-code
 az account set -s ea4faa5b-5e44-4236-91f6-5483d5b17d14
 az group create -n llm-runners -l eastus --tags owner=suriyak SkipAutoDeleteTill=2024-12-31
-az container create -n testacr3 --restart-policy Always --image suriyakalivardhan/testimage:v1 --secure-environment-variables abcd=xyz  --command-line "python myrunner.py -n=60"
+
+
+az container create -n llm-runner-eastus --restart-policy Always --image suriyakalivardhan/llm-runner:v4 --secure-environment-variables OPENAI_API_KEY=abc AZURE_OPENAI_API_KEY=def --command-line "python runner.py -r=EastUS -m gpt4t0125" --assign-identity /subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourcegroups/llm-runners/providers/Microsoft.ManagedIdentity/userAssignedIdentities/llm-runner
